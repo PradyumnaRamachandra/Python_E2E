0e9c91a47d9d0d7ba9787a8050bad37219391166
Pradyumna R <prady07@gmail.com>
2020-04-06 16:23:40 +0530

    updated

---
+++ TestSuite.py
@@ -0,0 +1,19 @@
+import HtmlTestRunner
+import os
+import unittest
+
+test_suite={
+    "Login_Module":'yes',
+    "Module2":'',
+    "Module3":''
+
+}
+
+loader = unittest.TestLoader()
+suite = loader.discover("C:\\PythonProjects\\Python_E2E\\Testcases")
+runner=HtmlTestRunner.HTMLTestRunner(output="C:/PythonProjects/Python_E2E/reports")
+if suite:
+    runner.run(suite)
+
+if __name__=="__main__":
+    unittest.main()
---
+++ Testcases/test_SampleTest.py
@@ -0,0 +1,51 @@
+from selenium import webdriver
+import time
+from selenium.webdriver.support import expected_conditions as ec
+from selenium.webdriver.support.ui import WebDriverWait
+import unittest
+
+from Pages.HomePage import HomePage
+from Pages.LoginPage import LoginPage
+
+import HtmlTestRunner
+
+
+# Test Script
+class LoginTest(unittest.TestCase):
+
+        def setUp(self):
+            print("Test Case Started")
+
+
+
+        def test_loginTc1(self):
+            driver = webdriver.Chrome(executable_path="C:\\PythonProjects\\Python_E2E\\drivers\\chromedriver.exe")
+            driver.implicitly_wait(20)
+            driver.maximize_window()
+            driver.get("https://learn.letskodeit.com/p/practice")
+            login=LoginPage(driver)
+            login.Login()
+            homepage = HomePage(driver)
+            homepage.validate_homepage("abc@email.com","xxxx")
+            driver.close()
+
+
+
+        def test_loginTc2(self):
+            driver = webdriver.Chrome(executable_path="C:\\PythonProjects\\Python_E2E\\drivers\\chromedriver.exe")
+            driver.implicitly_wait(20)
+            driver.maximize_window()
+            driver.get("https://learn.letskodeit.com/p/practice")
+            login = LoginPage(driver)
+            login.Login()
+            homepage = HomePage(driver)
+            homepage.validate_homepage("abcde@email.com", "xxxx")
+            driver.close()
+
+
+        def tearDown(self):
+            print("Test Completed")
+
+
+
+
